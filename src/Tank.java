e7bb10fe4158e0c317542017625f9a3f90407a30
Nishesh Chakrapani <nishesh_chakrapani@outlook.com>
2017-05-12 08:29:19 -0700

    Turret movement and power meter working, projectile on click is broken

--- src/Tank.java
+++ src/Tank.java
@@ -11,6 +11,7 @@ import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 
 /**
  * Created by nishu on 5/9/2017.
@@ -24,12 +25,16 @@ public class Tank {
     private DrawingPanel panel;
     private int xPos;
     private int yPos;
+    private int wind;
+    private Rectangle windBox;
+    private Color sky = new Color(135, 206 ,250);
     private BufferedImage bomb = ImageIO.read(new File("bomb.png"));
     private Point[] turretPts = {new Point(163, 475), new Point(158, 465), new Point(162, 465), new Point(160, 451),
     new Point(158, 447), new Point(158, 447), new Point(156, 436), new Point(154, 431), new Point(150, 428),
     new Point(145, 424), new Point(143, 419), new Point(138, 415), new Point(133, 412)};
     public Tank(int xPos, int yPos) throws IOException {
-        this.panel = new DrawingPanel(1000, 800);
+        this.panel = new DrawingPanel(2500, 800);
+        panel.setBackground(sky);
         this.g = panel.getGraphics();
         g2 = (Graphics2D)g;
         this.xPos = xPos;
@@ -37,11 +42,21 @@ public class Tank {
         g.drawImage(ImageIO.read(new File("tanksprite1.png")), xPos, yPos, null);
         Color brown = new Color(139, 69, 19);
         g.setColor(brown);
-        g.fillRect(0, 780, 1000, 20);
+        g.fillRect(0, 780, 2500, 20);
         g.drawImage(ImageIO.read(new File("Powerbar\\Powerbar1.png")), turretPts[0].x, turretPts[0].y, null);
-        fire(250, 40);
+        wind = getRandomWind();
+        if (wind > 0) {
+            g.drawImage(ImageIO.read(new File("WindLeft.png")), 1200, 50, null);
+        } else {
+            g.drawImage(ImageIO.read(new File("WindRight.png")), 1200, 50, null);
+        }
+        g.setFont(new Font("Large", Font.BOLD, 30));
+        g.setColor(Color.black);
+        g.drawString(Integer.toString(Math.abs(wind)), 1230, 43);
+        windBox = new Rectangle(1200, 10, 100, 80);
+        //fire(350, 200);
         panel.addMouseListener(new MouseInputAdapter() {
-            public void mousePressed(MouseEvent e) {
+            public void mouseClicked(MouseEvent e) {
                 fired = true;
                 Point p1 = new Point(xPos+144, yPos);
                 Point p2 = new Point(e.getX(), e.getY());
@@ -54,7 +69,11 @@ public class Tank {
                     vY = 350;
                 }
 
-                fire(vX, vY);
+                try {
+                    fire(vX, vY);
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
                 panel.sleep(3000);
                 fired = false;
             }
@@ -77,7 +96,7 @@ public class Tank {
                     }
 
                     int imgIndex = ((int) turretAngle / 5) + 1;
-                    g.setColor(Color.white);
+                    g.setColor(sky);
                     g.fillRect(xPos, yPos, 170, 70);
                     try {
                         g.drawImage(ImageIO.read(new File("TankSprite" + imgIndex + ".png")), xPos, yPos, null);
@@ -113,31 +132,54 @@ public class Tank {
         });
     }
 
-    private void fire(double vX, double vY) {
-        System.out.println("Firing with x velocity " + vX + " and y velocity " + vY);
-        g.setColor(Color.white);
+    private void fire(double vX, double vY) throws IOException {
+        g.setColor(sky);
         g.fillRect(133, 412, 300, 350);
         double deltaT = 0.01;
         double x = xPos+144;
         double y = yPos;
         double oldX = x;
         double oldY = y;
-        Rectangle ground = new Rectangle(0, 780, 1000, 20);
+        Rectangle ground = new Rectangle(0, 780, 10000, 20);
         Rectangle ball = new Rectangle((int)xPos, (int)yPos, 32, 32);
         while (!ball.intersects(ground)) {
             g.drawImage(bomb, (int)x, (int)y, null);
             ball = new Rectangle((int)x, (int)y, 32, 32);
             if (ball.intersects(ground)) {
+                panel.sleep(1000);
+                g.fillRect((int)x, (int)y, 32, 30);
                 break;
             }
-            vY += (G*deltaT);
+            if (ball.intersects(windBox)) {
+                if (wind > 0) {
+                    g.drawImage(ImageIO.read(new File("WindLeft.png")), 1200, 50, null);
+                } else {
+                    g.drawImage(ImageIO.read(new File("WindRight.png")), 1200, 50, null);
+                }
+                g.setFont(new Font("Large", Font.BOLD, 30));
+                g.setColor(Color.black);
+                g.drawString(Integer.toString(Math.abs(wind)), 1230, 43);
+            }
+            vX -= ((wind*1.1)*deltaT);
+            vY += ((G-(wind*1.1))*deltaT);
             x += (vX*deltaT);
             y -= (vY*deltaT);
             panel.sleep(10);
-            g.setColor(Color.white);
+            g.setColor(sky);
             g.fillRect((int)oldX, (int)oldY, 32, 32);
             oldX = x;
             oldY = y;
+        }
+    }
+
+    private int getRandomWind() {
+        Random rand = new Random();
+        int wind = rand.nextInt(25)+1;
+        int direction = rand.nextInt(2);
+        if (direction == 0) {
+            return wind;
+        } else {
+            return (wind*-1);
         }
     }
 }
